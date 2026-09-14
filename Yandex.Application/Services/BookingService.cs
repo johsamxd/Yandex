a79e026dad02b71006d139b4e0c9cce51d8b5866
Aman Kyrdzhagasov <aa.kyrdzhagasov@albank.ru>
2026-09-14 11:51:06 +0900

    Add lock for booking

--- Yandex.Application/Services/BookingService.cs
+++ Yandex.Application/Services/BookingService.cs
@@ -2,6 +2,7 @@
 using Yandex.Application.Abstractions;
 using Yandex.Application.Dtos.Bookings;
 using Yandex.Application.Exceptions;
+using Yandex.Application.Exceptions.Booking;
 using Yandex.Domain.Abstractions;
 using Yandex.Domain.Entities;
 
@@ -12,27 +13,44 @@ public class BookingService(
     IEntityRepository<Event> eventRepository,
     IMapper mapper) : IBookingService
 {
+    private readonly object _bookingLock = new();
+
     public async Task<BookingDto> CreateBookingAsync(Guid eventId)
     {
-        var existingEvent = eventRepository.GetById(eventId);
-        if (existingEvent is null) throw new NotFoundException($"Event with id: {eventId} not found");
-
-        var data = new Booking
-        {
-            EventId = eventId,
-        };
-
-        bookingRepository.Add(data);
-
         await Task.Delay(100);
+        
+        lock (_bookingLock)
+        {
+            var existingEvent = eventRepository.GetById(eventId);
+            if (existingEvent is null)
+            {
+                throw new NotFoundException($"Event with id: {eventId} not found");
+            }
 
-        return mapper.Map<BookingDto>(data);
+            if (!existingEvent.TryReserveSeats())
+            {
+                throw new NoAvailableSeatsException("No available seats for this event");
+            }
+
+            var data = new Booking
+            {
+                EventId = eventId,
+            };
+
+            bookingRepository.Add(data);
+            eventRepository.Update(existingEvent);
+
+            return mapper.Map<BookingDto>(data);
+        }
     }
 
     public async Task<BookingDto> GetBookingByIdAsync(Guid bookingId)
     {
         var data = bookingRepository.GetById(bookingId);
-        if (data is null) throw new NotFoundException($"Booking with id: {bookingId} not found");
+        if (data is null)
+        {
+            throw new NotFoundException($"Booking with id: {bookingId} not found");
+        }
 
         await Task.Delay(100);
 
